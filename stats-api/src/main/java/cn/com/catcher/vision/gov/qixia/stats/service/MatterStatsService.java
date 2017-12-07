afaa0715ec0c84ed0dfc5b86658b283f4db692db
listen <648770126@qq.com>
2017-12-07 08:41:22 +0800

    mm

--- stats-api/src/main/java/cn/com/catcher/vision/gov/qixia/stats/service/MatterStatsService.java
+++ stats-api/src/main/java/cn/com/catcher/vision/gov/qixia/stats/service/MatterStatsService.java
@@ -1,7 +1,6 @@
 package cn.com.catcher.vision.gov.qixia.stats.service;
 
 public interface MatterStatsService {
-
 	String findLastMatterStats( int count);
 	String getMatterStatsCountByGroup(int state,String groupIds);
 }
--- stats-api/src/main/java/cn/com/catcher/vision/gov/qixia/stats/service/UserService.java
+++ stats-api/src/main/java/cn/com/catcher/vision/gov/qixia/stats/service/UserService.java
@@ -1,6 +1,5 @@
 package cn.com.catcher.vision.gov.qixia.stats.service;
 
 public interface UserService {
-
 	String getUserCountInfo();
 }
